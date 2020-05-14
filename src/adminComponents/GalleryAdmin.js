79d3eb7c3c175ba031190e79aa31cad00edce78a
franco-canedo <francanedo1@hotmail.com>
2020-05-13 20:35:04 -0500

    image upload works

--- src/adminComponents/GalleryAdmin.js
+++ src/adminComponents/GalleryAdmin.js
@@ -1,10 +1,32 @@
 import React, { Component } from "react";
 import axios from 'axios';
 import { API_ROOT, HEADERS } from '../constants';
+import { connect } from 'react-redux';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 class GalleryAdmin extends Component {
     state = {
-        selectedFile: null
+        selectedFile: null,
+        photos: []
+    }
+
+    componentDidMount() {
+        // axios.get(`${API_ROOT}/api/v1/gallery`)
+        //     .then(res => {
+        //         console.log(res);
+        //         this.setState({
+        //             photos: [res]
+        //         })
+        //     })
+        axios.get(`${API_ROOT}/allPhotos`)
+            .then(res => {
+                console.log(res.data);
+                this.setState({
+                    photos: res.data
+                })
+            })
+
     }
 
     fileSelectedHandler = (event) => {
@@ -14,22 +36,95 @@ class GalleryAdmin extends Component {
     }
 
     uploadHandler = (event) => {
+        event.preventDefault();
+        if (this.state.selectedFile === null) {
+            alert('no file chosen');
+        } else {
+            const fd = new FormData();
+            // fd.append('photos', this.state.selectedFile, this.state.selectedFile.name);
+            // axios.patch(`${API_ROOT}/api/v1/update`, fd)
+            //     .then(res => {
+            //         console.log(res);
+            //         alert('file uploaded');
+            //         this.setState(prevState => {
+            //             return {
+            //                 photos: [...prevState.photos, res]
+            //             }
+            //         })
+            //     }).catch(error => alert(error));
+            fd.append('avatar', this.state.selectedFile, this.state.selectedFile.name);
+            fd.append('user_id', this.props.user.currentUser.id)
+            axios.post(`${API_ROOT}/photo`, fd)
+                .then(res => {
+                    console.log(res);
+                    alert('file uploaded');
+                    this.setState(prevState => {
+                        return {
+                            photos: [...prevState.photos, res.data]
+                        }
+                    })
+                }).catch(error => alert(error));
+
+        }
+
+    }
+
+    deletePicture = (event) => {
         const fd = new FormData();
-        fd.append('photos', this.state.selectedFile, this.state.selectedFile.name);
-        axios.patch(`${API_ROOT}/api/v1/update`, fd)
-        .then(res => console.log(res))
-        // .then(data => console.log(data))
+        fd.append('id', event.target.id)
+        axios.post(`${API_ROOT}/deletePhoto`, fd)
+            .then(res => {
+                alert('photo succesfully deleted')
+                const photo = this.state.photos.find(p => {
+                    return p.id === res.data.id
+                });
+                const newArray = this.state.photos.filter(p => p.id !== photo.id)
+                this.setState(prevState => {
+                    return {
+                        photos: newArray
+                    }
+                })
+            }).catch(error => alert(error));
     }
 
     render() {
         return (
             <div>
-
-                <div className="container">
-
+                <div className="containerAdmin">
                     <h1>Gallery</h1>
-                    <input type="file" onChange={this.fileSelectedHandler}></input>
-                    <button onClick={this.uploadHandler}>Upload</button>
+                    <div className="formGallery">
+                        <Form>
+                            <Form.Group controlId="formBasicEmail">
+                                <Form.Label>Upload a Picture</Form.Label>
+                                <Form.Control type="file" onChange={this.fileSelectedHandler} />
+                                <Form.Text className="text-muted">
+                                    Upload a .png file
+                            </Form.Text>
+                            </Form.Group>
+
+                            <Form.Group controlId="formBasicPassword">
+                                <Form.Label>Home Address</Form.Label>
+                                <Form.Control type="text" placeholder="Home Address" />
+                            </Form.Group>
+                            <Button variant="success" type="submit" onClick={this.uploadHandler}>
+                                Upload
+                        </Button>
+                        </Form>
+                    </div>
+                    {/* <input type="file" onChange={this.fileSelectedHandler}></input>
+                    <button onClick={this.uploadHandler}>Upload</button> */}
+                    <div className="photosContainer">
+                        {
+                            this.state.photos.map(photo => {
+                                return <div className="pictureCard">
+                                    <img id={photo.id} key={photo.id} alt="user pictures" src={photo.photo}
+                                        height="200" width="200"
+                                    ></img>
+                                    <Button variant="dark" onClick={this.deletePicture} id={photo.id}>X</Button>
+                                </div>
+                            })
+                        }
+                    </div>
                 </div>
             </div>
 
@@ -37,4 +132,10 @@ class GalleryAdmin extends Component {
     }
 }
 
-export default GalleryAdmin;
+const mapStateToProps = state => {
+    return {
+        user: state.user
+    }
+}
+
+export default connect(mapStateToProps, null)(GalleryAdmin);
