032cc581a2fce2c7dfafc337ab47ffef0b18266a
franco-canedo <francanedo1@hotmail.com>
2020-05-25 17:07:17 -0500

    aws url form added to all other components

--- src/adminComponents/HomeAdmin.js
+++ src/adminComponents/HomeAdmin.js
@@ -29,60 +29,86 @@ class HomeAdmin extends Component {
         })
     }
 
-    handleSubmit = (event) => {
-        event.preventDefault();
+    handleSubmit = async e => {
+        e.preventDefault();
         if (this.state.selectedFile.length === 0) {
             alert('no file chosen');
         } else {
+            const file = this.state.selectedFile;
+            if (!file) return;
+
+            
+
+            const payload = await fetch(`${API_ROOT}/s3/direct_post`).then(res =>
+                res.json()
+            );
+
+            const url = payload.url;
+            const formData = new FormData();
+
+            Object.keys(payload.fields).forEach(key =>
+                formData.append(key, payload.fields[key])
+            );
+            formData.append('file', file);
+
+            const xml = await fetch(url, {
+                method: 'POST',
+                body: formData
+            }).then(res => res.text());
+
+            const uploadUrl = new DOMParser()
+                .parseFromString(xml, 'application/xml')
+                .getElementsByTagName('Location')[0].textContent;
             const fd = new FormData();
             fd.append('photo', this.state.selectedFile);
+            fd.append('url', uploadUrl);
             axios.post(`${API_ROOT}/api/v1/update`, fd)
-            .then(res => {
-                console.log(res.data);
-                alert('New logo updated');
-                this.setState({
-                    avatar: res.data.avatar
-                })
-            }).catch(error => alert(error));            
+                .then(res => {
+                    console.log(res.data);
+                    alert('New logo updated');
+                    this.setState({
+                        avatar: res.data.avatar
+                    })
+                }).catch(error => alert(error));
         }
     }
 
     render() {
         return (
             <div>
-            <div className="container">
+                <div className="container">
 
-                <h1>Home</h1>
-                <div className="aboutForm">
-                    {
-                        this.state.edit ?
-                            <Fragment>
-                                <Form>
+                    <h1>Home</h1>
+                    <div className="aboutForm">
+                        {
+                            this.state.edit ?
+                                <Fragment>
+                                    <Form>
 
-                                    <Form.Group controlId="exampleForm.ControlTextarea1">
-                                        <Form.Label>Select a file to upload</Form.Label>
-                                        <Form.Control type="file" onChange={this.handleSelectedFile} />
-                                        <Form.Text className="text-muted">
-                                            Upload a .png file.
+                                        <Form.Group controlId="exampleForm.ControlTextarea1">
+                                            <Form.Label>Select a file to upload</Form.Label>
+                                            <Form.Control type="file" onChange={this.handleSelectedFile} />
+                                            <Form.Text className="text-muted">
+                                                Upload a .png file.
                                         </Form.Text>
-                                    </Form.Group>
-                                </Form>
-                                <Button variant="dark" onClick={this.editClick}>Cancel</Button>
-                                <Button variant="primary" onClick={this.handleSubmit} className="buttonMargin">Upload</Button>
-                            </Fragment> :
-                            <Button variant="dark" onClick={this.editClick}>Edit Logo</Button>
-                    }
+                                        </Form.Group>
+                                    </Form>
+                                    <Button variant="dark" onClick={this.editClick}>Cancel</Button>
+                                    <Button variant="primary" onClick={this.handleSubmit} className="buttonMargin">Upload</Button>
+                                </Fragment> :
+                                <Button variant="dark" onClick={this.editClick}>Edit Logo</Button>
+                        }
 
-                    
-                </div>
-                <div className="aboutInfo">
-                <img src={
-                        this.state.avatar === "" ? 
-                        this.props.user.currentUser.avatar :
-                        this.state.avatar
+
+                    </div>
+                    <div className="aboutInfo">
+                        <img src={
+                            this.state.avatar === "" ?
+                                this.props.user.currentUser.avatar :
+                                this.state.avatar
                         } alt="logo" className="homeLogoAdmin"></img>
-                </div>
-            </div >
+                    </div>
+                </div >
             </div>
         );
     }
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_ROOT = 'https://ancient-thicket-61232.herokuapp.com';
+export const API_ROOT = 'https://eclat-homes.herokuapp.com';
 export const HEADERS = {
     'Content-Type': 'application/json',
     Accept: 'application/json',
