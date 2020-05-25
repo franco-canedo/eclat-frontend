54f2da24c4014fc5932007c702295ee3a2e9c170
franco-canedo <francanedo1@hotmail.com>
2020-05-25 16:11:47 -0500

    root url cganged

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_ROOT = 'http://localhost:3000/';
+export const API_ROOT = 'https://ancient-thicket-61232.herokuapp.com';
 export const HEADERS = {
     'Content-Type': 'application/json',
     Accept: 'application/json',
